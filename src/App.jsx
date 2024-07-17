b83f3e158d119aa216598100e91900a3edec5416
sohamtapse <tapsesoham0@gmail.com>
2024-07-17 13:22:10 +0530

    add all file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar.jsx'
+import Main from './components/Main.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
